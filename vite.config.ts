f2c3bf98a5c3e342e1ee848329e523d4dd52024b
abulo.hoo <abulo.hoo@gmail.com>
2023-03-08 15:52:27 +0800

    配置短路径@符号

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
+import path from 'path'
 export default defineConfig({
+    resolve: {
+        //设置别名
+        alias: {
+            '@': path.join(__dirname, './src')
+        }
+    },
     plugins: [vue()]
 })
